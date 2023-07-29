b66fa90d87775f43f1761a57d7b99549290d50ea
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2023-07-29 22:28:07 +0900

    ssr設定更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   css: ["@/assets/main.scss", "vuetify/lib/styles/main.sass"],
   vite: {
     ssr: {
-      noExternal: ["vuetify"],
+      noExternal: [/\.css$/, /^vuetify/],
     },
   },
 });
