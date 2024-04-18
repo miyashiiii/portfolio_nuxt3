63a76ab6bcb60870d4339ede81b9b54da0d2564a
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-04-19 08:55:45 +0900

    Worksページ追加

--- pages/works.vue
+++ pages/works.vue
@@ -1,5 +1,37 @@
 <template>
-
+  <div class="q-pa-md row justify-center">
+    <div style="max-width: 950px">
+      <div
+        class="q-pa-md row items-start q-gutter-lg"
+        :class="$q.screen.lt.md ? 'justify-around' : 'justify-between'"
+      >
+        <q-card
+          v-for="post in works"
+          :href="post.url"
+          :style="$q.screen.lt.md ? 'width:75%' : 'width: 280px;'"
+          class=""
+        >
+          <a :href="post.url" class="row items-center justify-center bg-indigo-1">
+              <q-img :src="post.thumbnail" fit="scale-down" style="height:150px"/>
+          </a>
+          <q-card-section>
+            <a
+              :href="post.url"
+              class="text-bold text-black"
+              style="text-decoration: none"
+              >{{ post.title }}</a
+            >
+            <div class="q-mt-xs text-grey-8">{{ post.subtitle }}</div>
+            <q-chip
+              v-for="tag in post.tags"
+              class="text-caption q-ml-none"
+              :label="tag"
+            />
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,4 +40,27 @@ definePageMeta({
 });
 
 
+const works = [
+  {
+    title: "ポートフォリオ",
+    subtitle: "このサイト",
+    thumbnail: "../works/miyashiiii.png",
+    url: "/",
+    tags: ["Nuxt", "Quasar", "GitHub Pages"],
+  },
+  {
+    title: "数読",
+    subtitle: "Androidアプリ",
+    thumbnail: "../works/sudoku.png",
+    url: "https://play.google.com/store/apps/details?id=com.miyashiiii.sudoku",
+    tags: ["Android", "Flutter", "Dart"],
+  },
+  {
+    title: "TapNum",
+    subtitle: "Androidアプリ",
+    thumbnail: "../works/tapnum.png",
+    url: "https://play.google.com/store/apps/details?id=com.miyashiiii.tapnum",
+    tags: ["Android", "Unity", "C#"],
+  },
+];
 </script>
