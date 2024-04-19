c3a0ae491d55c3d0b386a2beb28a060e8352431b
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-04-19 19:13:37 +0900

    ナビゲーションバースマホ向け調整 途中

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
+  <!-- TODO heightの指定を画面高さに合わせる -->
+  <q-layout view="lHh lpr lFf" container style="height: 500px">
     <AppHeader />
-    <slot />
-  </div>
+    <q-page-container>
+      <slot />
+    </q-page-container>
+    <AppFooter />
+  </q-layout>
 </template>
