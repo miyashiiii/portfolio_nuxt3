7d1c66603b8d3a93ad130d615c10bf5ee6148eee
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-04-19 18:27:20 +0900

    postsページスマホ向け調整

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="q-pa-md row justify-center">
     <div style="max-width: 950px">
-      <div
-        class="q-pa-md row items-start q-gutter-lg"
-        :class="$q.screen.lt.md ? 'justify-around' : 'justify-between'"
-      >
-        <q-card
-          v-for="post in posts"
-          :href="post.url"
-          :style="$q.screen.lt.md ? 'width:75%' : 'width: 280px;'"
-          class=""
-        >
+      <!-- PC向け -->
+      <div class="q-pa-md row items-start q-gutter-lg gt-sm justify-between">
+        <q-card v-for="post in posts" :href="post.url" style="width: 280px">
           <a
             :href="post.url"
             class="row items-center justify-center bg-grey-1"
@@ -37,7 +30,8 @@
               {{ post.title }}
             </a>
             <div class="q-mt-xs text-grey-8">
-              {{ post.date }} - {{ getServiceFromUrl(post.url).name }}
+              <span class="text-black">{{ post.date }}</span> -
+              {{ getServiceFromUrl(post.url).name }}
             </div>
             <q-chip
               v-for="tag in post.tags"
@@ -46,6 +40,51 @@
             />
           </q-card-section>
         </q-card>
+      </div>
+
+      <!-- スマホ向け -->
+      <div class="q-pa-md row items-start q-gutter-lg lt-md">
+        <q-list separator>
+          <a
+            v-for="post in posts"
+            :href="post.url"
+            style="text-decoration: none"
+          >
+            <q-item class="q-gutter-x-lg">
+              <div
+                style="width: 120px; height: 80px"
+                class="row justify-center items-center bg-grey-1"
+              >
+                <q-img
+                  v-if="post.img"
+                  :src="getImgPath(post.img)"
+                  fit="scale-down"
+                  style="height: 80px"
+                />
+                <q-img
+                  v-else
+                  :src="getServiceFromUrl(post.url).icon"
+                  style="height: 50px; width: 50px"
+                />
+              </div>
+              <div class="col">
+                <div class="text-black text-bold">
+                  {{ post.title }}
+                </div>
+                <div class="q-mt-xs text-grey-8">
+                  <span class="text-black">{{ post.date }}</span> -
+                  {{ getServiceFromUrl(post.url).name }}
+                </div>
+                <q-chip
+                  v-for="tag in post.tags"
+                  class="text-caption q-ml-none"
+                  :label="tag"
+                />
+              </div>
+            </q-item>
+            <q-separator color="grey-4" />
+          </a>
+        </q-list>
       </div>
     </div>
   </div>
