664f4f41defa880653409a941f909e2952366229
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-04-19 12:01:24 +0900

    postsに画像を追加

--- pages/posts.vue
+++ pages/posts.vue
@@ -4,15 +4,12 @@
       
       <div class="q-pa-md row items-start q-gutter-lg" :class="$q.screen.lt.md ? 'justify-around': 'justify-between'">
         <q-card v-for="post in posts"  :href="post.url" :style="$q.screen.lt.md ? 'width:75%':'width: 280px;'" class="">
-        <a :href="post.url">
-          <q-card-section
-            class="row items-center justify-center bg-indigo-1"
-          >
-            <img
+        <a :href="post.url" class="row items-center justify-center bg-grey-1" style="height:120px">
+          <q-img v-if="post.img" :src="getImgPath(post.img)"fit="scale-down" style="height:120px"/>
+            <q-img v-else
               :src="getIconPath(post.url)"
               style="height: 50px; width: 50px"
             />
-          </q-card-section>
             </a>
           <q-card-section>
         <a :href="post.url" class="text-bold text-black" style="text-decoration: none;">{{ post.title }}</a>
@@ -33,12 +30,24 @@
 definePageMeta({
   layout: "default",
 });
+type Post = {
+  title: string;
+  url: string;
+  date: string;
+  tags: string[];
+  img: string;
+};
 import postsJson from "./assets/posts.json";
 
 const posts = ref(postsJson);
 
 const getDomain = (url: string) => {
   return new URL(url).hostname;
+};
+
+
+const getImgPath = (imgName:string) => {
+  return `/posts/${imgName}`;
 };
 
 const getIconPath = (url: string) => {
@@ -55,7 +64,7 @@ const getIconPath = (url: string) => {
   } else if (domain.includes("docswell")) {
     return "/sns/docswell.png";
   } else {
-    return "./sns/miyashiiii.png";
+    return "/sns/miyashiiii.png";
   }
 };
 </script>
