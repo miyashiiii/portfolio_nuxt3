9d0a99b1b35992d1533c8279740d3b0c7e9becc3
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-04-20 11:16:36 +0900

    トップページリンクをフッターに追加

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,36 +1,37 @@
 <template>
-  <!-- TODO heightを設定しないと要素が表示されない -->
+  <!-- SP向けのみ -->
   <q-footer
-    class="text-white row justify-center"
-    style="background-color: #5694b3"
+    class="text-white lt-md"
+    style="background-color: #5694b3; height: 50px"
   >
-    <!-- PC向け -->
-
-    <!-- SP向け -->
-    <div class="lt-md">
-      <q-toolbar>
-        <q-tabs
-          no-caps
-          indicator-color="white"
-          class="text-white"
-          v-model="tab"
-          switch-indicator
-        >
-          <NuxtLink to="/posts" style="text-decoration: none; color: inherit">
-            <q-tab name="posts">
-              <q-icon name="description" />
-              Posts
-            </q-tab>
-          </NuxtLink>
-          <NuxtLink to="/works" style="text-decoration: none; color: inherit">
-            <q-tab name="works">
-              <q-icon name="palette" />
-              Works
-            </q-tab>
-          </NuxtLink>
-        </q-tabs>
-      </q-toolbar>
-    </div>
+    <q-toolbar class="row justify-center">
+      <q-tabs
+        no-caps
+        indicator-color="white"
+        class="text-white"
+        v-model="tab"
+        switch-indicator
+      >
+        <NuxtLink to="/" style="text-decoration: none; color: inherit">
+          <q-tab name="index">
+            <q-icon name="home" />
+            Top
+          </q-tab>
+        </NuxtLink>
+        <NuxtLink to="/posts" style="text-decoration: none; color: inherit">
+          <q-tab name="posts">
+            <q-icon name="description" />
+            Posts
+          </q-tab>
+        </NuxtLink>
+        <NuxtLink to="/works" style="text-decoration: none; color: inherit">
+          <q-tab name="works">
+            <q-icon name="palette" />
+            Works
+          </q-tab>
+        </NuxtLink>
+      </q-tabs>
+    </q-toolbar>
   </q-footer>
 </template>
 
